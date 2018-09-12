e9f022b58199b5f66422f867a09db9f64a8ebbad
pezholio <pezholio@gmail.com>
2018-09-12 09:47:27 +0100

    PG, not SQLite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_12_082128) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "evidences", force: :cascade do |t|
     t.string "title"
     t.string "description"
