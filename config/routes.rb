e1a679456df6ef028fa311c49a9ffbfb9d479397
pezholio <pezholio@gmail.com>
2018-09-21 16:23:44 +0100

    Fix bad merge

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   namespace :admin do
-    resources :evidences
     resources :interventions
+    
+    root to: 'interventions#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
