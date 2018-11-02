4fe272f259bcd8358a9346ee19f566f8a44c2732
pezholio <pezholio@gmail.com>
2018-11-02 10:28:14 +0000

    Link to pages from header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protected
+  expose :pages, -> { Page.all }
 
   def after_sign_in_path_for(_resource)
     request.env['omniauth.origin'] || admin_root_path
