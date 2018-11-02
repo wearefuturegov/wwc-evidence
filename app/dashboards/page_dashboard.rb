bd93e44756353f3291e229400239fc906bcff24a
pezholio <pezholio@gmail.com>
2018-11-02 10:27:42 +0000

    Allow page to be edited in admin section

---
+++ app/dashboards/page_dashboard.rb
@@ -0,0 +1,44 @@
+require 'administrate/base_dashboard'
+
+class PageDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    title: Field::String,
+    body: Field::SimpleMDEMarkdown
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :title
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :title
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    title
+    body
+  ].freeze
+
+  # Overwrite this method to customize how subjects are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(subject)
+  #   "Subject ##{subject.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :interventions
     resources :tags
+    resources :pages
 
     root to: 'interventions#index'
   end
