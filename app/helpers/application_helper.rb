b2dfd2a9652c3daa5590dde9ba0fa8aaef9cac83
pezholio <pezholio@gmail.com>
2018-11-07 13:43:27 +0000

    Add latest tweet to footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def twitter
+    tweet = WwcEvidence::Twitter.instance.latest_tweet
+    return nil unless tweet
+
+    content_tag :div, class: 'twitter' do
+      output = content_tag(:span, link_to(tweet.attrs[:full_text], tweet.uri.to_s), id: 'tweet_cont')
+      output << content_tag(:span, link_to('View Tweet', tweet.uri.to_s), id: 'tweet_link')
+      output
+    end
+  end
 end
