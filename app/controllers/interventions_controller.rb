9b8eef6a0aab9048c797582b90544667f77f7f65
pezholio <pezholio@gmail.com>
2018-11-08 17:26:13 +0000

    Order interventions alphabetically

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,6 +1,6 @@
 class InterventionsController < ApplicationController
   expose :intervention, -> { Intervention.find(params[:id]).decorate }
-  expose :interventions, -> { Intervention.all.decorate }
+  expose :interventions, -> { Intervention.all.order(:title).decorate }
 
   def index; end
 
