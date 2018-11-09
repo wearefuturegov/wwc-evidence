484b32a37e2170e04b662942521bb145261bd5d3
pezholio <pezholio@gmail.com>
2018-11-09 10:29:08 +0000

    Use translations in enum fields

--- app/fields/enum_field.rb
+++ app/fields/enum_field.rb
@@ -5,9 +5,14 @@ class EnumField < Administrate::Field::Base
     data
   end
 
+  def key_translation(klass, key, value)
+    class_name = klass.to_s.downcase
+    I18n.t("helpers.label.#{class_name}.#{key}.#{value}")
+  end
+
   def select_field_values(form_builder)
     form_builder.object.class.public_send(attribute.to_s.pluralize).keys.map do |v|
-      [v.titleize, v]
+      [key_translation(form_builder.object.class, attribute.to_s, v), v]
     end
   end
 end
