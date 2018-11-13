da59d76824172c2d87392640686b0a31cd95deb1
pezholio <pezholio@gmail.com>
2018-11-13 10:36:37 +0000

    List files against intervention

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Intervention < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_many_attached :files
   has_many :outcomes
   has_many :links
@@ -15,6 +17,19 @@ class Intervention < ApplicationRecord
   attr_accessor :files_to_delete
 
   after_update :delete_files
+
+  def file_list
+    files.map do |f|
+      OpenStruct.new(
+        title: f.filename.to_s,
+        url: rails_blob_path(f, only_path: true)
+      )
+    end
+  end
+
+  def files_and_links
+    (file_list + links).sort_by(&:title)
+  end
 
   private
 
--- spec/models/intervention_spec.rb
+++ spec/models/intervention_spec.rb
@@ -31,4 +31,24 @@ RSpec.describe Intervention, type: :model do
   it 'can have many tags' do
     expect(intervention.tags).to eq(tags)
   end
+
+  context 'with attached files' do
+    let(:files) { generate_files(3) }
+
+    before do
+      files.each_with_index do |f, i|
+        intervention.files.attach(io: File.open(f), filename: "file-#{i}.txt")
+      end
+    end
+
+    it 'gives a list of files' do
+      expect(intervention.file_list.count).to eq(3)
+      expect(intervention.file_list.first.title).to eq('file-0.txt')
+      expect(intervention.file_list.first.url).to match(%r{rails\/active_storage\/blobs\/})
+    end
+
+    it 'combines links and files' do
+      expect(intervention.files_and_links.count).to eq(intervention.links.count + files.count)
+    end
+  end
 end
