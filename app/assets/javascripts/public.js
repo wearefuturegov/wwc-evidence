dddc9f47c9b9284e48aaa179b519f18fdf690302
Chris Evans <evanschris@users.noreply.github.com>
2018-11-23 15:12:26 +0000

    update sub page
    add in back link
    make contents sticky
    update infobox

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
-  var originalTop = $(".sticky-nav").offset().top;
+  if ($(".sticky-nav").length) {
+    var originalTop = $(".sticky-nav").offset().top;
+  }
+  if ($(".sticky-contents").length) {
+    var originalTop = $(".sticky-contents").offset().top - 25;
+    var bottomPoint = originalTop + $('.intervention_main_content').height() - $('.sticky-contents').height() - 25;
+  }
 
-   $(window).scroll(function(){
-    if ($(window).scrollTop() > originalTop) {
-      $(".sticky-nav").addClass("sticky");
-      $(".sticky-inner").addClass("sticky-bulk");
-    } else {
-      $(".sticky-nav").removeClass("sticky");
-      $(".sticky-inner").removeClass("sticky-bulk");
+
+  $(window).scroll(function(){
+    var scrollPoint = $(window).scrollTop();
+
+    if ($(".sticky-nav").length) {
+      homePageScroll(scrollPoint, originalTop);
+    }
+    if ($(".sticky-contents").length) {
+      subPageScroll(scrollPoint, originalTop, bottomPoint);
     }
   });
 });
+
+
+function homePageScroll(scrollPoint, originalTop) {
+  if (scrollPoint > originalTop) {
+    $(".sticky-nav").addClass("sticky");
+    $(".sticky-inner").addClass("sticky-bulk");
+  } else {
+    $(".sticky-nav").removeClass("sticky");
+    $(".sticky-inner").removeClass("sticky-bulk");
+  }
+}
+function subPageScroll(scrollPoint, originalTop, bottomPoint) {
+  $('.sticky-contents .sticky-inner').width($(".sticky-contents").width());
+
+  if (scrollPoint > originalTop) {
+    $(".sticky-contents").addClass("sticky");
+    if (scrollPoint > bottomPoint) {
+      $(".sticky-contents").addClass('reached_bottom');
+      $('.sticky-inner').css('top', (bottomPoint-$('.sticky-contents').height() + 25));
+    } else {
+      $(".sticky-contents").removeClass('reached_bottom');
+      $('.sticky-inner').css('top', 25);
+    }
+  } else {
+    $(".sticky-contents").removeClass("sticky");
+  }
+}
